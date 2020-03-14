30f2108b13e5848e42275019ebf20d111248848d
Peter Fernandes <supersonicandtails@gmail.com>
2020-03-14 15:34:10 -0400

    Rename audit-results-view to audit-details

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { LogView } from "./log-view.js";
 import { AuditResultsList as AuditResultsList } from "./audit-results-list.js";
 import { AuditResult, Audit, Line, LineWithTimeStamp } from "./audit.js";
-import { AuditResultView } from "./audit-result-view.js";
+import { AuditDetailsView } from "./audit-details.js";
 
 let dirHandle: FileSystemDirectoryHandle;
 const fileListEntries = [] as FileSystemHandle[];
@@ -13,7 +13,7 @@ patternInput.onchange = () => renderFileList();
 let selectedFileHandle: FileSystemHandle;
 let logView: LogView;
 let auditResultsList: AuditResultsList;
-let auditResultView: AuditResultView;
+let auditResultView: AuditDetailsView;
 
 async function onPageLoad()
 {
@@ -48,7 +48,7 @@ async function onPageLoad()
 	logView.messageStartPattern = /\d{1,4}-\d{1,2}-\d{1,2}\s+\d{1,2}:\d{1,2}:\d{1,2},\d{3}/; // timestamp
 
 	auditResultsList = document.querySelector("audit-results-list") as AuditResultsList;
-	auditResultView = document.querySelector("audit-result") as AuditResultView;
+	auditResultView = document.querySelector("audit-result") as AuditDetailsView;
 }
 
 function renderFileList()
