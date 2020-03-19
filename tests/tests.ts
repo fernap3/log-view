b08a25adfdad0b7abed15f6feeb3cc8dcc6ce21f
Peter Fernandes <supersonicandtails@gmail.com>
2020-03-18 20:35:32 -0400

    Add (failing) test

--- tests/tests.ts
+++ tests/tests.ts
@@ -1,9 +1,20 @@
 import { registerTest } from "astr";
+import { runAudits } from "../bin/audit.js";
+import * as fs from "fs";
 
 registerTest({
 	name: "One equals one",
 	func: async (assert) =>
 	{
-		assert.equals(1, 1);
+		const logText = fs.readFileSync("test-logs/ExagoStephen.log", { encoding: "utf8" });
+		
+		const audits = [];
+
+		for await (let audit of runAudits(logText))
+			audits.push(audit);
+
+		
+		// make some assertion about audits
+		throw "NOT IMPLEMENTED";
 	}
 });
